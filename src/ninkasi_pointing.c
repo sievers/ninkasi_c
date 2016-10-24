a76cdb99c89394bb57662ab81e71617b5c201d3b
Jonathan Sievers <jonathan.sievers@gmail.com>
2016-10-24 14:33:37 +0200

    fixed cbass version of polarized preconditioner projection operator

--- src/ninkasi_pointing.c
+++ src/ninkasi_pointing.c
@@ -1643,7 +1643,7 @@ void precalc_actpol_pointing_exact(mbTOD *tod, int op_flag)
     ACTpolState_init(state);
 
     ACTpolScan scan;
-#if 1
+#if 0
     tod->actpol_pointing->alt0=0.8446;
     tod->actpol_pointing->az0=5.204;
     tod->actpol_pointing->az_throw=0.1307;
